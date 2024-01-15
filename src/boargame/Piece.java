e6ae4f3c930e3bcbe00bebf4194f0a4174c7535a
Ediney Mendonça <edineyjr1@gmail.com>
2024-01-15 11:18:54 -0300

    Start to implement Board and Piece

--- src/boargame/Piece.java
+++ src/boargame/Piece.java
@@ -1,4 +1,24 @@
 package boargame;
 
-public class Piece {
+public abstract class Piece {
+    protected Position position;
+    private  Board  board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public abstract Piece[][] possibleMoves();
+
+    public boolean possibleMove(Position position) {
+        return true;
+    }
+
+    public boolean isThereAnyPossibleMove() {
+        return true;
+    }
 }
