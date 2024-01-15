3e70c8c427378e5806a84cf9f00d275a8b2560ad
Ediney Mendonça <edineyjr1@gmail.com>
2024-01-15 11:08:55 -0300

    First class: Position

--- src/boargame/Position.java
+++ src/boargame/Position.java
@@ -1,4 +1,29 @@
 package boargame;
 
 public class Position {
+    private Integer row;
+    private Integer column;
+
+    public Position(Integer row, Integer column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setValues(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d, %d", row, column);
+    }
 }
