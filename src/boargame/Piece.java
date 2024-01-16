7aa8d96047cf7d5e03967b0005c415e90b685aa7
Ediney Mendonça <edineyjr1@gmail.com>
2024-01-16 10:45:19 -0300

    Possible moves of a Piece

--- src/boargame/Piece.java
+++ src/boargame/Piece.java
@@ -1,6 +1,6 @@
 package boargame;
 
-public class Piece {
+public abstract class Piece {
     protected Position position;
     private  Board  board;
 
@@ -12,15 +12,20 @@ public class Piece {
         return board;
     }
 
-    public Piece[][] possibleMoves() {
-        return null;
-    }
+    public abstract boolean[][] possibleMoves();
 
     public boolean possibleMove(Position position) {
-        return true;
+        return  possibleMoves()[position.getRow()][position.getColumn()];
     }
 
     public boolean isThereAnyPossibleMove() {
-        return true;
+        boolean[][] mat = possibleMoves();
+        for (int i=0; i < mat.length; i++) {
+            for (int j=0; j < mat.length; j++) {
+                if (mat[i][j])
+                    return true;
+            }
+        }
+        return false;
     }
 }
